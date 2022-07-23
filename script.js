b561e3caf57b0071c0ecda415f5a9cf1161f705c
ilhomjonSolijonov <ilhomjon206solijonov@gmail.com>
2022-07-23 22:41:00 +0500

    version 1 of Booking Hotels

---
+++ script.js
@@ -0,0 +1,91 @@
+// carousel 1
+$('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay:true,
+    autoplayTimeOut: 10000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 1,
+            nav: false
+        },
+        1000:{
+            items: 1,
+            nav: false
+        }
+    }
+});
+// carousel 2
+$('.carousel2').owlCarousel({
+    margin: 30,
+    loop: true,
+    autoplay:true,
+    autoplayTimeOut: 10000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+    }
+});
+//  carousel 3
+$('.carousel3').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay:true,
+    autoplayTimeOut: 10000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+    }
+});
+
+//  carousel 4
+$('.carousel4').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay:true,
+    autoplayTimeOut: 10000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 3,
+            nav: false
+        },
+        600:{
+            items: 4,
+            nav: false
+        },
+        1000:{
+            items: 6,
+            nav: false
+        }
+    }
+});
+
+ 
